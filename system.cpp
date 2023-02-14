d04d970b15ebd7d4351adb27dc0e3876fbdc2df1
Jakub Kłos <93655086+Klosiu889@users.noreply.github.com>
2023-02-14 13:14:12 +0100

    Poprawki

--- system.cpp
+++ system.cpp
@@ -184,6 +184,7 @@ System::System(machines_t machines, unsigned int numberOfWorkers,
 }
 
 std::vector<WorkerReport> System::shutdown() {
+    if (!is_open) return std::move(reports);
     reports.clear();
 
     std::unique_lock<std::mutex> lock(pending_orders_mutex);
